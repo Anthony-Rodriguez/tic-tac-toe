974c0d7a9b3c1e3cfb931b6397ac547ba227a76a
Anthony-Rodriguez <7Anthony.Rodriguez7@gmail.com>
2020-12-12 14:02:13 -0500

    Add check for winner function
    Added code to check for game cells
    Added function to check for win conditions

--- assets/scripts/authorized/game/events.js
+++ assets/scripts/authorized/game/events.js
@@ -1,18 +1,15 @@
 'use strict'
 const api = require('./api')
 const ui = require('./ui')
-const store = require('./../../store')
+// const store = require('./../../store')
 let numberOfTurns = 0
 
 const onGameStart = function (event) {
   event.preventDefault()
   api.gameStart()
     .then(ui.gameStartSuccess)
+    .then(ui.boardRevealSuccess)
     .catch(ui.failure)
-}
-const onBoardReveal = function (event) {
-  event.preventDefault()
-  ui.boardRevealSuccess()
 }
 const onCellUpdate = function (event) {
   const cellIndex = $(this).data('cell-index')
@@ -27,11 +24,13 @@ const onCellUpdate = function (event) {
   } else {
     player = players.player2
   }
+  console.log(player)
   if (($(cell).css('background-image') === 'none')) {
     api.cellUpdate(cellIndex, player)
-      .then(ui.cellUpdateSuccess(player))
-    numberOfTurns++
-    console.log(store.game.cells)
+      .then(function (response) {
+        ui.cellUpdateSuccess(response, player, event)
+        numberOfTurns++
+      })
   } else {
     $('#message').html('<div><h2>Choose an empty tile!</h2></div>')
   }
@@ -39,7 +38,6 @@ const onCellUpdate = function (event) {
 
 module.exports = {
   onGameStart,
-  onBoardReveal,
   onCellUpdate,
   numberOfTurns
 }
--- assets/scripts/authorized/game/ui.js
+++ assets/scripts/authorized/game/ui.js
@@ -3,16 +3,18 @@ const store = require('./../../store')
 const failure = function (error) {
   $('#message').text(error.responseJSON.message)
 }
-const boardRevealSuccess = function () {
-  $('.container').show()
-  $('#game-start').show()
-  $('#board-reveal').hide()
-}
+
 const gameStartSuccess = function (response) {
   $('#message').html("<div><h2>Let's begin with player </h2><img src='public/letter-x-img.png'></div>")
+  $('#game-restart').show()
+  $('#game-start').hide()
+  $('.container').show()
   store.game = response.game
 }
-const cellUpdateSuccess = function (player) {
+const cellUpdateSuccess = function (response, player, event) {
+  store.game = response.game
+  const space = (store.game.cells)
+  console.log(space)
   const picX = ({
     'background-image': "url('public/letter-x-img.png')",
     'background-position': 'center',
@@ -29,15 +31,49 @@ const cellUpdateSuccess = function (player) {
   if (player === 'X') {
     $(myBox).css(picX)
     $('#message').html("<div><h2>It's your turn, player </h2><img src='public/letter-o-img.png'></div>")
-  } else if (player === 'O') {
+    if (space[0] === 'X' && space[1] === 'X' && space[2] === 'X') {
+      $('#message').html("<div><h2>The winner is Player </h2><img src='public/letter-x-img.png'></div>")
+    } else if (space[3] === 'X' && space[4] === 'X' && space[5] === 'X') {
+      $('#message').html("<div><h2>The winner is Player </h2><img src='public/letter-x-img.png'></div>")
+    } else if (space[6] === 'X' && space[7] === 'X' && space[8] === 'X') {
+      $('#message').html("<div><h2>The winner is Player </h2><img src='public/letter-x-img.png'></div>")
+    } else if (space[0] === 'X' && space[3] === 'X' && space[6] === 'X') {
+      $('#message').html("<div><h2>The winner is Player </h2><img src='public/letter-x-img.png'></div>")
+    } else if (space[1] === 'X' && space[4] === 'X' && space[7] === 'X') {
+      $('#message').html("<div><h2>The winner is Player </h2><img src='public/letter-x-img.png'></div>")
+    } else if (space[2] === 'X' && space[5] === 'X' && space[8] === 'X') {
+      $('#message').html("<div><h2>The winner is Player </h2><img src='public/letter-x-img.png'></div>")
+    } else if (space[0] === 'X' && space[4] === 'X' && space[8] === 'X') {
+      $('#message').html("<div><h2>The winner is Player </h2><img src='public/letter-x-img.png'></div>")
+    } else if (space[2] === 'X' && space[4] === 'X' && space[6] === 'X') {
+      $('#message').html("<div><h2>The winner is Player </h2><img src='public/letter-x-img.png'></div>")
+    }
+  }
+  if (player === 'O') {
     $(myBox).css(picO)
     $('#message').html("<div><h2>It's your turn, player </h2><img src='public/letter-x-img.png'></div>")
+    if (space[0] === 'O' && space[1] === 'O' && space[2] === 'O') {
+      $('#message').html("<div><h2>The winner is Player </h2><img src='public/letter-o-img.png'></div>")
+    } else if (space[3] === 'O' && space[4] === 'O' && space[5] === 'O') {
+      $('#message').html("<div><h2>The winner is Player </h2><img src='public/letter-o-img.png'></div>")
+    } else if (space[6] === 'O' && space[7] === 'O' && space[8] === 'O') {
+      $('#message').html("<div><h2>The winner is Player </h2><img src='public/letter-o-img.png'></div>")
+    } else if (space[0] === 'O' && space[3] === 'O' && space[6] === 'O') {
+      $('#message').html("<div><h2>The winner is Player </h2><img src='public/letter-o-img.png'></div>")
+    } else if (space[1] === 'O' && space[4] === 'O' && space[7] === 'O') {
+      $('#message').html("<div><h2>The winner is Player </h2><img src='public/letter-o-img.png'></div>")
+    } else if (space[2] === 'O' && space[5] === 'O' && space[8] === 'O') {
+      $('#message').html("<div><h2>The winner is Player </h2><img src='public/letter-o-img.png'></div>")
+    } else if (space[0] === 'O' && space[4] === 'O' && space[8] === 'O') {
+      $('#message').html("<div><h2>The winner is Player </h2><img src='public/letter-o-img.png'></div>")
+    } else if (space[2] === 'O' && space[4] === 'O' && space[6] === 'O') {
+      $('#message').html("<div><h2>The winner is Player </h2><img src='public/letter-o-img.png'></div>")
+    }
   }
 }
 
 module.exports = {
   failure,
   gameStartSuccess,
-  boardRevealSuccess,
   cellUpdateSuccess
 }
